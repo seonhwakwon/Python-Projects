850dbe81154881058593b00c8b3317cbd971928a
sunnakwon <148311845+sunnakwon@users.noreply.github.com>
2024-01-13 12:48:17 -0800

    Inheritance Class assignment

--- Inheritance/inheritance.py
+++ Inheritance/inheritance.py
@@ -1,17 +1,24 @@
+#parent class 
 class Person:
+  # initialize method
   def __init__(self):
     self.name ="Seonhwa"
     self.age = 26
-  
+
+#child class instance  
 class Korean(Person):
+
+  #initialize method
   def __init__(self):
+    #call pearents method
     super().__init__()
     self.lang ='Korean'
     self.food ='Kimchi'
 
+  #introduce method
   def introduce(self):
     print(f"My name is {self.name}, and I am {self.age} years old.\nMy native language is {self.lang}, and My favorite food is {self.food}.")  
 
+#invoke the method of Korean()
 Person2 = Korean()
-
 Person2.introduce()
